0db9ada04b21652a368931d1ed1ae30b23a33444
Anson Li <ansonli.94@gmail.com>
2015-10-20 17:52:34 -0600

    Sample SQL implementation

--- src/FlightBooker.java
+++ src/FlightBooker.java
@@ -1,4 +1,7 @@
-//import sampleui?
+import java.util.*;
+import java.sql.*; // Java package for accessing Oracle
+import java.io.*; // Java package includes Console for getting password from user and printing to screen
+
 public class FlightBooker {
   
   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
@@ -6,8 +9,27 @@ public class FlightBooker {
 
   public static void main(String[] args)
   {
-    final String db_username = System.getenv("DB_USERNAME"); // username and password is sent in by user at beginning...
-    final String db_password = System.getenv("DB_PASSWORD");
+  	// before you start, write 'CLASSPATH = $CLASSPATH:' into the console.
+  	// get username
+	System.out.print("Username: ");
+	Console co = System.console();
+	String m_userName = co.readLine();
+	
+	// obtain password
+	char[] passwordArray = co.readPassword("Password: ");
+	String m_password = new String(passwordArray);
+
+	// The URL we are connecting to
+    String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+
+	// The driver to use for connection
+    String m_driverName = "oracle.jdbc.driver.OracleDriver";
+
+    Connection m_con;
+    String createString;
+
+    // final String db_username = System.getenv("DB_USERNAME"); // username and password is sent in by user at beginning...
+    // final String db_password = System.getenv("DB_PASSWORD");
     /*
     Scanner scan = new Scanner(System.in);
     System.out.println("Login to database - Enter username:");
@@ -28,9 +50,27 @@ public class FlightBooker {
       //Handle errors for Class.forName
       e.printStackTrace();
     } */ 
-      UI cli = new UI();
 
+    try {
+	  Class drvClass = Class.forName(m_driverName);  
+    } catch (Exception e) {
+      System.err.print("ClassNotFoundException: ");
+      System.err.println(e.getMessage());
+    }
+	try {
+	  // Establish a connection
+   	  m_con = DriverManager.getConnection(m_url, m_userName, m_password);
+	  // Changed to reflect changes made in the result set and to make these changes permanent to the database too
+      Statement stmt = m_con.createStatement(
+		ResultSet.TYPE_SCROLL_SENSITIVE, 
+		ResultSet.CONCUR_UPDATABLE); 
+      UI cli = new UI(); // no statement passed - i think the sql passed here...
+	  // No more statements to compile/execute. So, close connection.
+      stmt.close();
+      m_con.close();
+	  System.out.println("SQL success.");
+   	} catch(SQLException ex) {
+      System.err.println("SQLException: " + ex.getMessage());
+    }
   }
-
-
 }
