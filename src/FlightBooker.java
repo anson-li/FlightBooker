f947e035db38f0481f12522c09cbb07f420b4e32
Satyen Akolkar <s.akolkar3@gmail.com>
2015-10-20 21:35:54 -0600

    clean up

--- src/FlightBooker.java
+++ src/FlightBooker.java
@@ -1,76 +1,47 @@
 import java.util.*;
 import java.sql.*; // Java package for accessing Oracle
-import java.io.*; // Java package includes Console for getting password from user and printing to screen
+import java.io.Console; // Java package includes Console for getting password from user and printing to screen
 
 public class FlightBooker {
-  
-  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+
+  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
   static final String DB_URL = "jdbc:mysql://localhost/EMP";
 
   public static void main(String[] args)
   {
   	// before you start, write 'CLASSPATH = $CLASSPATH:' into the console.
-  	// get username
-	System.out.print("Username: ");
-	Console co = System.console();
-	String m_userName = co.readLine();
-	
-	// obtain password
-	char[] passwordArray = co.readPassword("Password: ");
-	String m_password = new String(passwordArray);
 
-	// The URL we are connecting to
-    String m_url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
-
-	// The driver to use for connection
-    String m_driverName = "oracle.jdbc.driver.OracleDriver";
-
-    Connection m_con;
-    String createString;
-
-    // final String db_username = System.getenv("DB_USERNAME"); // username and password is sent in by user at beginning...
-    // final String db_password = System.getenv("DB_PASSWORD");
-    /*
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Login to database - Enter username:");
-    db_username = scan.nextLine();
-    System.out.println("Login to database - Enter password:");
-    db_password = scan.nextLine();
-    try {
-
-      Class.forName("com.mysql.jdbc.Driver");
-      System.out.println("Connecting to database...");
-      Connection connect = DriverManager.getConnection(DB_URL,db_username,db_password);
-      UI cli = new UI();
-    
-    } catch(SQLException se) {
-      //Handle errors for JDBC
-      se.printStackTrace();
-    } catch(Exception e) {
-      //Handle errors for Class.forName
-      e.printStackTrace();
-    } */ 
-
-    try {
-	  Class drvClass = Class.forName(m_driverName);  
-    } catch (Exception e) {
-      System.err.print("ClassNotFoundException: ");
-      System.err.println(e.getMessage());
+    Console co = System.console();
+    if (co == null)
+    {
+      System.err.println("System.console() returned null.")
+      // System.console() returns null if console doesn't exist
+      // need error handling
     }
-	try {
-	  // Establish a connection
-   	  m_con = DriverManager.getConnection(m_url, m_userName, m_password);
-	  // Changed to reflect changes made in the result set and to make these changes permanent to the database too
-      Statement stmt = m_con.createStatement(
-		ResultSet.TYPE_SCROLL_SENSITIVE, 
-		ResultSet.CONCUR_UPDATABLE); 
-      UI cli = new UI(); // no statement passed - i think the sql passed here...
-	  // No more statements to compile/execute. So, close connection.
-      stmt.close();
-      m_con.close();
-	  System.out.println("SQL success.");
-   	} catch(SQLException ex) {
-      System.err.println("SQLException: " + ex.getMessage());
+
+    String m_userName = "";
+    String m_password = "";
+
+    System.println("Login in to SQLPlus Database")
+    // get username
+    try {
+    	m_userName = co.readLine("SQLPlus Username: ");
+      char[] passwordArray = co.readPassword("SQLPlus Password: ");
+    	m_password = String.valueOf(passwordArray);
+    } catch (IOError ioe) {
+      // System.err.println(ioe.toString());
+    }
+
+    try {
+      SQLHandler sql_handler = SQLHandler(m_userName, m_password);
+
+      UI cli = new UI();
+
+      // No more statements to compile/execute. So, close connection.
+      sql_handler.close();
+      System.out.println("SQL success.");
+   	} catch(SQLException sqle) {
+      System.err.println("SQLException: " + sqle.getMessage());
     }
   }
 }
--- src/SQLHandler.java
+++ src/SQLHandler.java
@@ -7,7 +7,7 @@ public class SQLHandler
   private String username;
   private String password;
   private Statement statement;
-  
+
   SQLHandler(String username, String password) throws SQLException
   {
     try {
@@ -15,38 +15,47 @@ public class SQLHandler
     } catch (ClassNotFoundException cnfe) {
       System.err.println("Could not load driver.");
     }
-    
+
     setURL();
     this.username = username;
     this.password = password;
-    
+
     makeConnection();
+
+    // Changed to reflect changes made in the result set and to make these changes permanent to the database too
+    statement = con.createStatement( ResultSet.TYPE_SCROLL_SENSITIVE,
+                                            ResultSet.CONCUR_UPDATABLE);
   }
-  
+
   private void loadDriver() throws ClassNotFoundException
   {
     String driver_name = "oracle.jdbc.driver.OracleDriver";
     /*Class driver_class =*/ Class.forName(driver_name);
   }
-  
+
   private void setURL()
   {
-    url = System.getenv("DB_URL");
+    url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS" // or System.getenv("DB_URL");
   }
-  
+
   private void makeConnection() throws SQLException
   {
     con = DriverManager.getConnection(url, username, password);
-    this.statement = con.createStatement();
   }
-  
+
   public void runSQLStatement(String s) throws SQLException
   {
     statement.executeUpdate(s);
   }
-  
+
   public ResultSet runSQLQuery(String q) throws SQLException
   {
     return statement.executeQuery(q);
+  }
+
+  public void close()
+  {
+    statement.close();
+    con.close();
   }
 }
