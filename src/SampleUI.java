2f008797d5a5297d616fee6351b009332c6aef27
Anson Li <ansonli.94@gmail.com>
2015-10-25 00:20:09 -0600

    isinteger function

--- src/SampleUI.java
+++ src/SampleUI.java
@@ -513,4 +513,16 @@ class UI
     Matcher m = p.matcher(pword);
     return m.matches();
   }
+
+  public static boolean isInteger(String s, int radix) {
+    if(s.isEmpty()) return false;
+    for(int i = 0; i < s.length(); i++) {
+        if(i == 0 && s.charAt(i) == '-') {
+            if(s.length() == 1) return false;
+            else continue;
+        }
+        if(Character.digit(s.charAt(i),radix) < 0) return false;
+    }
+    return true;
+}
 }
