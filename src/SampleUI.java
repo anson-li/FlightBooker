376b03fead218ee0df62afafa7a50a73cb60cb9f
Anson Li <ansonli.94@gmail.com>
2015-10-24 23:23:04 -0600

    formatting to fix view problems

--- src/SampleUI.java
+++ src/SampleUI.java
@@ -28,25 +28,25 @@ class UI
 
   public void GenerateViews() throws SQLException {
     String dropAvailableFlights = "drop view available_flights";
-    String createAvailableFlights = "create view available_flights(flightno,dep_date,src,dst,dep_time," +
-      "arr_time,fare,seats,price) as" +                         
-      "select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))," +
+    String createAvailableFlights = "create view available_flights(flightno,dep_date,src,dst,dep_time, " +
+      "arr_time,fare,seats,price) as " +                         
+      "select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time)), " +
       "f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))+(f.est_dur/60+a2.tzone-a1.tzone)/24," +
-      "fa.fare, fa.limit-count(tno), fa.price" + 
-      "from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2" +
-      "where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and" +
-      "f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and" +
-      "sf.dep_date=b.dep_date(+)" + 
-      "group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone," +
-      "a1.tzone, fa.fare, fa.limit, fa.price" +
+      "fa.fare, fa.limit-count(tno), fa.price " + 
+      "from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2 " +
+      "where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and " +
+      "f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and " +
+      "sf.dep_date=b.dep_date(+) " + 
+      "group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone, " +
+      "a1.tzone, fa.fare, fa.limit, fa.price " +
       "having fa.limit-count(tno) > 0";
     String dropGoodConnections = "drop view good_connections";
-    String createGoodConnections = "create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as" +
-      "select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time," +
+    String createGoodConnections = "create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as " +
+      "select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time, " +
       "min(a1.price+a2.price) " +
-      "from available_flights a1, available_flights a2" +
-      "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time" +
-      "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time";
+      "from available_flights a1, available_flights a2 " +
+      "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time " +
+      "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time ";
 
     sql_handler.runSQLStatement(dropAvailableFlights);
     sql_handler.runSQLStatement(createAvailableFlights);
