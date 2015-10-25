55060f705aa754b2db44212a9c420ce12f67f632
Satyen Akolkar <s.akolkar3@gmail.com>
2015-10-24 23:42:56 -0600

    creating views

--- src/SampleUI.java
+++ src/SampleUI.java
@@ -29,39 +29,35 @@ class UI
   };
 
   public void GenerateViews() throws SQLException {
-	  
-	  /*
-    String dropAvailableFlights = "if exists (select * from all_tables where table_name like 'AVAILABLE_FLIGHTS')" +
-      "drop table available_flights";
-    String createAvailableFlights = "create view available_flights(flightno,dep_date,src,dst,dep_time," +
-      "arr_time,fare,seats,price) as" +
-      "select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))," +
-      "f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))+(f.est_dur/60+a2.tzone-a1.tzone)/24," +
-      "fa.fare, fa.limit-count(tno), fa.price" +
-      "from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2" +
-      "where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and" +
-      "f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and" +
-      "sf.dep_date=b.dep_date(+)" +
-      "group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone," +
-      "a1.tzone, fa.fare, fa.limit, fa.price" +
-      "having fa.limit-count(tno) > 0";
-    String dropGoodConnections = "if exists (select * from all_tables where table_name like 'GOOD_CONNECTIONS')" +
-      "drop table good_connections";
-    String createGoodConnections = "create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as" +
-      "select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time," +
-      "min(a1.price+a2.price) " +
-      "from available_flights a1, available_flights a2" +
-      "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time" +
-      "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time";
-	*/
-	System.out.println("@generate_views");
-	sql_handler.runSQLStatement("@generate_views");
-	//String dropAvailableFlights = "drop table available_flights" ;
-	//String createAvailableFlights = 
-    //sql_handler.runSQLStatement(dropAvailableFlights);
-    //sql_handler.runSQLStatement(createAvailableFlights);
+
+
+    String dropAvailableFlights = "drop table available_flights";
+    String createAvailableFlights = "create view available_flights(flightno,dep_date, src,dst,dep_time,arr_time,fare,seats," +
+    		"price) as " +
+    		"select f.flightno, sf.dep_date, f.src, f.dst, f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time)),"+
+    		" f.dep_time+(trunc(sf.dep_date)-trunc(f.dep_time))+(f.est_dur/60+a2.tzone-a1.tzone)/24,"+
+    		"       fa.fare, fa.limit-count(tno), fa.price"+
+    		"from flights f, flight_fares fa, sch_flights sf, bookings b, airports a1, airports a2 "+
+    		"where f.flightno=sf.flightno and f.flightno=fa.flightno and f.src=a1.acode and "+
+    		"f.dst=a2.acode and fa.flightno=b.flightno(+) and fa.fare=b.fare(+) and "+
+    		"sf.dep_date=b.dep_date(+) "+
+    		"group by f.flightno, sf.dep_date, f.src, f.dst, f.dep_time, f.est_dur,a2.tzone, "+
+    		"a1.tzone, fa.fare, fa.limit, fa.price "+
+    		"having fa.limit-count(tno) > 0";
+    // String dropGoodConnections = "if exists (select * from all_tables where table_name like 'GOOD_CONNECTIONS')" +
+    //   "drop table good_connections";
+    // String createGoodConnections = "create view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as" +
+    //   "select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time," +
+    //   "min(a1.price+a2.price) " +
+    //   "from available_flights a1, available_flights a2" +
+    //   "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time" +
+    //   "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time";
+
+	sql_handler.runSQLStatement(dropAvailableFlights);
+    sql_handler.runSQLStatement(createAvailableFlights);
     //sql_handler.runSQLStatement(dropGoodConnections);
     //sql_handler.runSQLStatement(createGoodConnections);
+	System.out.println("@generate_views");
   }
 
   public void WelcomeScreen() throws SQLException {
