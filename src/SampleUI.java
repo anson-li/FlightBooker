2170a1a0f5772a81f43bd2f9c9102ff48ee08c9a
Anson Li <ansonli.94@gmail.com>
2015-10-26 10:00:20 -0600

    syntax fix

--- src/SampleUI.java
+++ src/SampleUI.java
@@ -599,7 +599,7 @@ class UI
           GenerateViews();
           //String checkFlightExists = "select flightno from available_flights where flightno = '" + flightno1 "'";
           //no way to do queries in a transaction - use a catch { sql_handler.con.rollback(); }
-          String addToTickets = "insert into tickets values (" + maxTno + ", '" + name + "', '" pub_email + "', " + price + ")";
+          String addToTickets = "insert into tickets values (" + maxTno + ", '" + name + "', '" + pub_email + "', " + price + ")";
           sql_handler.runSQLStatement(addToTickets);
           String addToBookings = "insert into bookings values (" + maxTno + ", '" + flightno1 + "', '" + fare + "', '" + convdate + "', null)";
           sql_handler.runSQLStatement(addToBookings);
