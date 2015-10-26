cd5aaa0506ef5e5b327c3c2bf1cc22bce78ead45
Anson Li <ansonli.94@gmail.com>
2015-10-26 01:39:11 -0600

    debugging test

--- src/SampleUI.java
+++ src/SampleUI.java
@@ -563,7 +563,9 @@ class UI
         DateFormat initialdf = new SimpleDateFormat("yyyy-mm-dd");
         try {
           depdate = depdate.substring(0, 10);
+          System.out.println("Substring1 :  " + depdate);
           convdate = df.format(initialdf.parse(depdate));
+          System.out.println("convdate... " + convdate);
         } catch (ParseException e) { System.out.println(e); }
       }
       System.out.println("-----------------------------------------");
