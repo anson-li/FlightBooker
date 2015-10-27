ec8d664fea066196d92664c613b0ca2d87154bbc
Anson Li <ansonli.94@gmail.com>
2015-10-27 10:36:08 -0600

    Bugfix: format update for flights check

--- src/UI.java
+++ src/UI.java
@@ -1213,7 +1213,7 @@ class UI
   public static boolean isValidDate(String input) {
     boolean valid = false;
     try {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd hh:mm:ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd hh24:mi:ss");
         dateFormat.setLenient(false);
         java.util.Date output = dateFormat.parse(input);
         valid = true;
